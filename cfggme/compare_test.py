2d66a1395c13695653800a790a68c2509565e870
Emily Simons <152223142+emsimons@users.noreply.github.com>
2024-10-28 16:41:19 +0100

    testing comparator

---
+++ cfggme/compare_test.py
@@ -0,0 +1,41 @@
+from curvature import Curvature
+from compare import GraphComparator
+import networkx as nx
+import numpy as np
+
+def test_pytest():
+    assert 1 + 1 == 2
+
+def test_self():
+    curv = Curvature()
+    graph1 = nx.erdos_renyi_graph(100, 0.1)
+    graph2 = graph1
+    comp = GraphComparator(graph1, graph2, curv)
+    assert comp.calc_total_curv_diff() == 0
+
+def test_placeholder_comparison():
+    curv = Curvature()
+    graph1 = nx.erdos_renyi_graph(100, 0.2)
+    graph2 = nx.erdos_renyi_graph(100, 0.1)
+    comp = GraphComparator(graph1, graph2, curv)
+    assert comp.calc_total_curv_diff() != 0
+
+def test_same_landscape():
+    curv = Curvature()
+    graph1 = nx.erdos_renyi_graph(100, 0.1)
+    graph2 = graph1
+    comp = GraphComparator(graph1, graph2, curv)
+    assert comp.curvature_filtration_distance() == 0
+
+def test_diff_landscape():
+    curv = Curvature()
+    graph1 = nx.erdos_renyi_graph(100, 0.1)
+    graph2 = nx.erdos_renyi_graph(100, 0.15)
+    comp = GraphComparator(graph1, graph2, curv)
+    assert comp.curvature_filtration_distance() != 0
+
+    graph3 = nx.erdos_renyi_graph(100, 0.3)
+    comp2 = GraphComparator(graph1, graph3, curv)
+
+    #graphs 1 and 2 should have more similar curvature than graphs 1 and 3
+    assert comp.curvature_filtration_distance() < comp2.curvature_filtration_distance() 
