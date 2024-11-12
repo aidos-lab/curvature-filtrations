b0cec741f1fb19e12a0b9c6c6378a48e7275204b
Emily Simons <152223142+emsimons@users.noreply.github.com>
2024-11-12 11:17:57 +0100

    documenting test_representations.py

--- tests/topology/test_representations.py
+++ tests/topology/test_representations.py
@@ -7,17 +7,21 @@ from curvature_filtrations.topology.distances import LandscapeDistance
 
 
 class TestDiagram:
+    """Class designed to test the functionality of the PersistenceDiagram class."""
 
     def test_create_object(self):
+        """Test the creation of a simple PersistanceDiagram object."""
         diagram = PersistenceDiagram()
         assert type(diagram) == PersistenceDiagram
 
     def test_defaults(self):
+        """Test that the defaults are set correctly."""
         diagram = PersistenceDiagram()
         assert diagram.homology_dims == [0, 1]
         assert diagram.persistence_pts == None
 
     def test_set_persistence_pts(self, dummy_diagram):
+        """Test the setter and getter methods for attaching persistence points to the PersistenceDiagram object."""
         diagram = PersistenceDiagram()
         assert diagram._persistence_pts == None
         # setting persistence points
@@ -25,28 +29,35 @@ class TestDiagram:
         assert diagram.persistence_pts == dummy_diagram
 
     def test_get_pts_for_dim(self, dummy_diagram):
+        """Test retrieval of persistence points for a specific dimension."""
         diagram = PersistenceDiagram()
         # set and get
         diagram.persistence_pts = dummy_diagram
         assert np.all(diagram.get_pts_for_dim(0) == dummy_diagram[0])
 
     def test_ph_calc(self, graph):
+        """Test that fit_transform on a KILT object returns a PersistenceDiagram object."""
         klt = KILT()
         ph = klt.fit_transform(graph)
         assert type(ph) == PersistenceDiagram
 
 
 class TestLandscape:
+    """Class designed to test the functionality of the PersistenceLandscape class."""
+
     def test_create_object(self):
+        """Test instantiation of a PersistenceLandscape object."""
         pl = PersistenceLandscape()
         assert type(pl) == PersistenceLandscape
 
     def test_defaults(self):
+        """Test that the defaults are set correctly."""
         pl = PersistenceLandscape()
         assert pl.homology_dims == [0, 1]
         assert pl._functions == None
 
     def test_diagram_to_landscape(self, toy_pd):
+        """Test the method that converts a PersistenceDiagram in to a PersistenceLandscape."""
         assert type(toy_pd[0]) == PersistenceDiagram
         dist = LandscapeDistance(None, None)
         pl = dist._convert_to_landscape(toy_pd)[0]
@@ -54,6 +65,7 @@ class TestLandscape:
         assert pl.functions != None
 
     def test_avg_landscape(self, toy_pd, toy_pd2):
+        """Test the functionality for averaging multiple PersistenceLandscapes."""
         dist = LandscapeDistance(None, None)
         pl1 = dist._convert_to_landscape(toy_pd)[0]
         pl2 = dist._convert_to_landscape(toy_pd2)[0]
