cb66c946c8ec677a13beb459725977aaf2ddf836
Emily Simons <152223142+emsimons@users.noreply.github.com>
2024-11-18 14:55:15 +0100

    proper documentation

--- curvature_filtrations/topology/distances.py
+++ curvature_filtrations/topology/distances.py
@@ -12,8 +12,31 @@ from curvature_filtrations.topology.representations import (
 class TopologicalDistance(ABC):
     """Abstract base class for computing topological distances.
 
-    Takes in two persistence diagrams and handles necessary transformations
+    Takes in two persistence diagrams (or lists of persistence diagrams) and handles necessary transformations
     to compute a specific distance function.
+
+    Attributes
+    ----------
+    diagram 1 : PersistenceDiagram or List[PersistenceDiagram]
+        The PersistenceDiagram or List[PersistenceDiagram] to be compared with diagram2.
+    diagram 2 : PersistenceDiagram or List[PersistenceDiagram]
+        The PersistenceDiagram or List[PersistenceDiagram] to be compared with diagram1.
+    norm : int, default=2.
+        Defines what norm will be used for calculations. Default is 2.
+
+    Core Methods
+    -------
+    norm(x) -> float:
+        Returns the norm of vector x according to the order specified in the norm attribute.
+    _is_distribution(diagram) -> bool:
+        Returns True if input is a list (i.e. distribution), False otherwise.
+    supports_distribution() -> bool:
+        Returns True if the distance type supports comparing distributions of persistence diagrams.
+    fit -> avg1, avg2 :
+        Converts diagram1 and diagram2 into average topological descriptors.
+    transform(descriptor1, descriptor2) -> float:
+        Computes the distance between inputted topological descriptors and returns it as a float.
+
     """
 
     def __init__(self, diagram1, diagram2, norm=2) -> None:
@@ -54,9 +77,55 @@ class TopologicalDistance(ABC):
 
 
 class LandscapeDistance(TopologicalDistance):
-    """Computes distances between persistence landscapes."""
+    """
+    Takes in persistence diagrams, converts them to persistence landscapes, and computes the distance between them.
+    This TopologicalDistance subclass supports comparison of distributions (i.e. input of lists of persistence diagrams).
+
+    Attributes
+    ----------
+    diagram 1 : PersistenceDiagram or List[PersistenceDiagram]
+        The PersistenceDiagram or List[PersistenceDiagram] to be compared with diagram2.
+    diagram 2 : PersistenceDiagram or List[PersistenceDiagram]
+        The PersistenceDiagram or List[PersistenceDiagram] to be compared with diagram1.
+    norm : int, default=2.
+        Defines what norm will be used for calculations. Default is 2.
+    resolution : int, default=1000
+        The resolution/number of samples in each landscape function.
+    num_functions : int, default=5.
+        The number of landscape functions to be computed.
+    landscape_transformer : gudhi package object
+        The object that powers the transformation of persistence diagrams to persistence landscapes.
+
+    Core Methods
+    -------
+    supports_distribution() -> True :
+        Indicates that LandscapeDistance supports comparison between distributions.
+    fit() -> PersistenceLandscape, PersistenceLandscape:
+        Converts persistence diagrams into persistence landscapes.
+        If diagram1 and/or diagram2 are distributions, the average persistence landscapes for each distribution is returned.
+    transform(landscape1 : PersistenceLandsape, landscape2 : PersistenceLandscape) -> float :
+        Takes in two persistence landscapes and computes the distance between them.
+    fit_transform() -> float :
+        Runs fit() to create two persistence landscapes and then transform() to calculate the distance between them.
+    """
 
     def __init__(self, diagram1, diagram2, norm=2, resolution=1000, num_functions=5) -> None:
+        """
+        Creates a LandscapeDistance object for two persistence diagrams (or lists of persistence diagrams).
+
+        Parameters
+        ----------
+        diagram 1 : PersistenceDiagram or List[PersistenceDiagram]
+            The PersistenceDiagram or List[PersistenceDiagram] to be compared with diagram2.
+        diagram 2 : PersistenceDiagram or List[PersistenceDiagram]
+            The PersistenceDiagram or List[PersistenceDiagram] to be compared with diagram1.
+        norm : int, default=2.
+            Defines what norm will be used for calculations. Default is 2.
+        resolution : int, default=1000
+            The resolution/number of samples in each landscape function.
+        num_functions : int, default=5.
+            The number of landscape functions to be computed.
+        """
         super().__init__(diagram1, diagram2, norm)
         self.resolution = resolution
         self.num_functions = num_functions
@@ -64,12 +133,27 @@ class LandscapeDistance(TopologicalDistance):
             resolution=resolution, num_landscapes=num_functions
         )
 
+    #  ╭──────────────────────────────────────────────────────────╮
+    #  │ Core Methods                                             │
+    #  ╰──────────────────────────────────────────────────────────╯
+
     def supports_distribution(self) -> bool:
         """Indicates support for distributions of persistence diagrams."""
         return True
 
     def fit(self, **kwargs):
-        """Compute and return average persistence landscapes for both diagrams."""
+        """Computes and returns persistence landscapes from the persistence diagrams, diagram1 and diagram2 (or the average persistence landscapes, if diagram1 and/or diagram2 are distributions.
+
+        Returns
+        -------
+        avg1 : PersistenceLandscape
+            A wrapper object for the (average) persistence landscape for diagram1.
+            Attribute called 'functions' stores a Dict[int, np.array] that a maps homology dimension key to a np.array with shape (num_functions * resolution, 1) which concatenates the samples from all landscape functions.
+
+        avg2 : PersistenceLandscape
+            A wrapper object for the (average) persistence landscape for diagram1.
+            Attribute called 'functions' stores a Dict[int, np.array] that a maps homology dimension key to a np.array with shape (num_functions * resolution, 1) which concatenates the samples from all landscape functions.
+        """
         landscapes1 = self._convert_to_landscape(self.diagram1)
         landscapes2 = self._convert_to_landscape(self.diagram2)
 
@@ -80,7 +164,23 @@ class LandscapeDistance(TopologicalDistance):
     def transform(
         self, landscape1: PersistenceLandscape, landscape2: PersistenceLandscape
     ) -> float:
-        """Compute the norm-based distance between two persistence landscapes."""
+        """Computes the norm-based distance between two persistence landscapes.
+        Cannot be executed before fit() method has been run to generate persistence landscapes.
+
+        Parameters
+        ----------
+        landscape1 : PersistenceLandscape
+            A wrapper object for the first persistence landscape to be compared.
+            Attribute called 'functions' stores a Dict[int, np.array] that a maps homology dimension key to a np.array with shape (num_functions * resolution, 1) which concatenates the samples from all landscape functions.
+        landscape1 : PersistenceLandscape
+            A wrapper object for the second persistence landscape to be compared.
+            Attribute called 'functions' stores a Dict[int, np.array] that a maps homology dimension key to a np.array with shape (num_functions * resolution, 1) which concatenates the samples from all landscape functions.
+
+        Returns
+        -------
+        float :
+            The norm-based distance between the two given persistence landscapes.
+        """
         common_dims = set(landscape1.homology_dims).intersection(landscape2.homology_dims)
         difference = self._subtract_landscapes(landscape1, landscape2)
 
@@ -89,10 +189,14 @@ class LandscapeDistance(TopologicalDistance):
 
     # TODO: fit_transform method
 
+    #  ╭──────────────────────────────────────────────────────────╮
+    #  │ Helper Functions                                         │
+    #  ╰──────────────────────────────────────────────────────────╯
+
     def _convert_to_landscape(
         self, diagrams: List[PersistenceDiagram]
     ) -> List[PersistenceLandscape]:
-        """Convert each persistence diagram to a persistence landscape for each dimension."""
+        """Convert each persistence diagram to a persistence landscape."""
         landscapes = []
 
         for diagram in diagrams:
@@ -103,7 +207,6 @@ class LandscapeDistance(TopologicalDistance):
             )
             landscape_functions = {}
             for dim, points in diagram.persistence_pts.items():
-                # TODO: check this... H1 is not responding to changes in PD input
                 transformed_points = self.landscape_transformer.fit_transform([points])[0]
                 landscape_functions[dim] = transformed_points
             # set to PersistenceLandscape functions attribute
@@ -114,7 +217,7 @@ class LandscapeDistance(TopologicalDistance):
 
     @staticmethod
     def _average_landscape(landscapes: List[PersistenceLandscape]) -> PersistenceLandscape:
-        """Compute the average persistence landscape across multiple landscapes."""
+        """Compute and return the average persistence landscape across multiple landscapes."""
         avg_landscape = {}
         # sum landscape functions
         for landscape in landscapes:
@@ -138,16 +241,19 @@ class LandscapeDistance(TopologicalDistance):
         return avg_pl
 
     @staticmethod
-    # TODO: make it for common dimensions, not from landscape1
     def _subtract_landscapes(
         landscape1: PersistenceLandscape, landscape2: PersistenceLandscape
     ) -> PersistenceLandscape:
-        """Subtract two landscapes for each common dimension."""
+        """Subtract two landscapes for each common dimension, returning a PersistenceLandscape that represents the difference."""
+        assert (
+            landscape1.resolution == landscape2.resolution
+            and landscape1.num_functions == landscape2.num_functions
+        ), "Cannot subtract landscapes with different resolutions or number of landscape functions."
+        common_dims = set(landscape1.homology_dims).intersection(landscape2.homology_dims)
         # Initialize PersistenceLandscape to return
-        diff_pl = PersistenceLandscape(landscape1.homology_dims)
-
+        diff_pl = PersistenceLandscape(common_dims)
         diff_functions = {}
-        for dim in landscape1.homology_dims:
+        for dim in common_dims:
             diff_functions[dim] = landscape1.get_fns_for_dim(dim) - landscape2.get_fns_for_dim(dim)
         diff_pl.functions = diff_functions
         return diff_pl
@@ -157,6 +263,42 @@ class LandscapeDistance(TopologicalDistance):
 
 
 class ImageDistance(TopologicalDistance):
+    """
+    Takes in persistence diagrams, converts them to persistence images, and computes the distance between them.
+    This TopologicalDistance subclass supports comparison of distributions (i.e. input of lists of persistence diagrams).
+
+    Attributes
+    ----------
+    diagram 1 : PersistenceDiagram or List[PersistenceDiagram]
+        The PersistenceDiagram or List[PersistenceDiagram] to be compared with diagram2.
+    diagram 2 : PersistenceDiagram or List[PersistenceDiagram]
+        The PersistenceDiagram or List[PersistenceDiagram] to be compared with diagram1.
+    norm : int, default=2.
+        Defines what norm will be used for calculations. Default is 2.
+    bandwidth : double, default = 1.0
+        Controls the Gaussian kernel for the probability distribution calculated for each point on the birth/persistence diagram.
+        See gudhi documentation for more information.
+    weight : function, default = lambda x: 1
+        Defines the weight function used to compute the weighted sum of the probability distributions for each point on the bith/persistence diagram, i.e. the persistence surface.
+        Default is a constant function. Other common choices are a linear function or a bump function, which put greater emphasis on points with longer persistence.
+        See gudhi documentation for more information.
+    resolution : List[int, int], default = [20,20]
+        The dimensions of the persistence image in pixels.
+    image_transformer : gudhi package object
+        The object that powers the transformation of persistence diagrams into persistence images.
+
+    Methods
+    supports_distribution() -> True :
+        Indicates that ImageDistance supports comparison between distributions.
+    fit() -> PersistenceImage, PersistenceImage:
+        Converts persistence diagrams into persistence images.
+        If diagram1 and/or diagram2 are distributions, the average persistence image for each distribution is returned.
+    transform(image1 : PersistenceImage, image2 : PersistenceImage) -> float :
+        Takes in two persistence images and computes the distance between them.
+    fit_transform() -> float :
+        Runs fit() to create two persistence images and then transform() to calculate the distance between them.
+
+    """
 
     def __init__(
         self, diagram1, diagram2, norm=2, bandwidth=1.0, weight=lambda x: 1, resolution=[20, 20]
@@ -173,7 +315,24 @@ class ImageDistance(TopologicalDistance):
         """Indicates support for distributions of persistence images."""
         return True
 
+    #  ╭──────────────────────────────────────────────────────────╮
+    #  │ Core Methods                                             │
+    #  ╰──────────────────────────────────────────────────────────╯
+
     def fit(self, **kwargs):
+        """Computes and returns persistence images from the persistence diagrams, diagram1 and diagram2 (or the average persistence images, if diagram1 and/or diagram2 are distributions.
+
+        Returns
+        -------
+        avg1 : PersistenceImage
+            A wrapper object for the (average) persistence landscape for diagram1.
+            Attribute called 'pixels' stores a Dict[int, np.array] that a maps homology dimension key to a np.array that stores image data.
+
+        avg2 : PersistenceImage
+            A wrapper object for the (average) persistence landscape for diagram1.
+            Attribute called 'pixels' stores a Dict[int, np.array] that a maps homology dimension key to a np.array that stores image data.
+        """
+
         """Compute and return average persistence images for both diagrams."""
         img1 = self._convert_to_image(self.diagram1)
         img2 = self._convert_to_image(self.diagram2)
@@ -183,11 +342,31 @@ class ImageDistance(TopologicalDistance):
         return avg1, avg2
 
     def transform(self, image1, image2) -> float:
-        """Compute the norm-based distance between two persistence images."""
+        """Computes the norm-based distance between two persistence images.
+        Cannot be executed before fit() method has been run to generate persistence images.
+
+        Parameters
+        ----------
+        image1 : PersistenceImage
+            A wrapper object for the first persistence image to be compared.
+            Attribute called 'pixels' stores a Dict[int, np.array] that a maps homology dimension key to a np.array with shape (resolution[0] * resolution[1], 1), which contains the values of the persistence surface at each pixel.
+        image2 : PersistenceImage
+            A wrapper object for the second persistence image to be compared.
+            Attribute called 'pixels' stores a Dict[int, np.array] that a maps homology dimension key to a np.array with shape (resolution[0] * resolution[1], 1), which contains the values of the persistence surface at each pixel.
+
+        Returns
+        -------
+        float :
+            The norm-based distance between the two given persistence landscapes.
+        """
         common_dims = set(image1.homology_dims).intersection(image2.homology_dims)
         difference = self._subtract_images(image1, image2)
         distance = sum(self.norm(difference.pixels[dim]) for dim in common_dims)
         return distance
+
+    #  ╭──────────────────────────────────────────────────────────╮
+    #  │ Helper Functions                                         │
+    #  ╰──────────────────────────────────────────────────────────╯
 
     def _convert_to_image(self, diagrams: List[PersistenceDiagram]) -> List[PersistenceImage]:
         """Convert each persistence diagram in the given list into to a persistence image in the returned list."""
@@ -242,7 +421,7 @@ class ImageDistance(TopologicalDistance):
     def _subtract_images(
         self, image1: PersistenceImage, image2: PersistenceImage
     ) -> PersistenceImage:
-        """Subtract two images for each common dimension."""
+        """Subtracts two images for each common dimension, returning a PersistenceImage that represents the difference."""
         common_dims = set(image1.homology_dims).intersection(image2.homology_dims)
         assert (
             image1.resolution == image2.resolution
@@ -254,7 +433,6 @@ class ImageDistance(TopologicalDistance):
             weight=self.weight,
             resolution=self.resolution,
         )
-
         # subtraction
         diff_pixels = {}
         for dim in common_dims:
@@ -263,7 +441,8 @@ class ImageDistance(TopologicalDistance):
         return diff_pi
 
     def __str__(self):
-        pass
+        """Return user-friendly string representation of the ImageDistance instance."""
+        return f"ImageDistance object between (1) [{self.diagram1}] and (2) [{self.diagram2}]"
 
 
 class SilhouetteDistance(TopologicalDistance):
