add009011e3e3050c1c9a1758848203a24be7719
Emily Simons <152223142+emsimons@users.noreply.github.com>
2024-11-18 15:16:56 +0100

    testing fit_transform()

--- tests/topology/test_distances.py
+++ tests/topology/test_distances.py
@@ -44,6 +44,14 @@ class TestLandscapeDistance:
         distance = setup_landscape_distance.transform(avg1, avg2)
         assert isinstance(distance, float)
 
+    def test_fit_transform(self, toy_pd, toy_pd2):
+        LD = LandscapeDistance(toy_pd, toy_pd2, norm=2)
+        distance1 = LD.fit_transform()
+        avg1, avg2 = LD.fit()
+        distance2 = LD.transform(avg1, avg2)
+        assert type(distance2) == np.float64
+        assert distance1 == distance2
+
     def test_subtract_landscapes(self, setup_landscape_distance):
         """Test subtraction of landscapes."""
         landscapes1 = setup_landscape_distance._convert_to_landscape(
@@ -58,6 +66,11 @@ class TestLandscapeDistance:
         assert 1 in diff.functions
         assert isinstance(diff.functions[0], np.ndarray)
         assert isinstance(diff.functions[1], np.ndarray)
+
+    def test_same_pds(self, toy_pd):
+        same = toy_pd
+        LD = LandscapeDistance(toy_pd, same)
+        assert LD.fit_transform() == 0
 
 
 class TestImageDistance:
@@ -93,3 +106,16 @@ class TestImageDistance:
         ID = ImageDistance(toy_pd, toy_pd2, norm=2)
         avg1, avg2 = ID.fit()
         assert type(ID.transform(avg1, avg2)) == np.float64
+
+    def test_fit_transform(self, toy_pd, toy_pd2):
+        ID = ImageDistance(toy_pd, toy_pd2, norm=2)
+        distance1 = ID.fit_transform()
+        avg1, avg2 = ID.fit()
+        distance2 = ID.transform(avg1, avg2)
+        assert type(distance2) == np.float64
+        assert distance1 == distance2
+
+    def test_same_pds(self, toy_pd):
+        same = toy_pd
+        ID = ImageDistance(toy_pd, same)
+        assert ID.fit_transform() == 0
